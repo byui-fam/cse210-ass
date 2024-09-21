bd0f56579c65bc926598282b99ed0240657ba39b
byui-fad <false>
2024-09-21 23:01:57 +0100

    finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int favouriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favouriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+    //Display message
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    //Ask for user name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    //Ask for user favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    //Square the user number
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    //The result
+    static void DisplayResult(string name, int squareNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareNumber}");
     }
 }
